1ed3f7c7357fc41094df140fcd6d8210156a3bc4
Bryce <blim425@aucklanduni.ac.nz>
2023-09-13 01:56:58 +1200

    added 1year query

--- Query_Database.py
+++ Query_Database.py
@@ -15,35 +15,75 @@ conn = pyodbc.connect(connection_string)
 engine = sa.create_engine('mssql+pyodbc://', creator=lambda:conn)
 
 ########################################################################################################################
+if __name__ == '__main__':
+    ####CHANGE HERE####
+    OTABEN = False
+    ALLNODES = True
 
-Start_Date = '2010-01-01'
-End_Date = '2024-01-01'
+    if OTABEN:
+        # Set start End Dates
+        Start_Date = '2010-01-01'
+        End_Date = '2024-01-01'
+        # Get the POCs
+        POC_List = ['BEN2201', 'OTA0221']
 
-POC_List = ['BEN2201', 'OTA0221']
+        # Query
+        with engine.begin() as conn:
+            # For now let us only take Otahuhu and Benmore
+            sql_query = f'''
+            SELECT *
+            FROM FinalEnergyPrices
+            WHERE TradingDate BETWEEN '{Start_Date}' AND '{End_Date}'
+            AND PointOfConnection IN ({', '.join(["'"+item+"'" for item in POC_List])})'''
+            print("Querying...")
+            results = conn.execute(sa.text(sql_query)).fetchall()
+            print("Finished Query")
 
-with engine.begin() as conn:
-    # For now let us only take Otahuhu and Benmore
-    sql_query = f'''
-    SELECT *
-    FROM FinalEnergyPrices
-    WHERE TradingDate BETWEEN '{Start_Date}' AND '{End_Date}'
-    AND PointOfConnection IN ({', '.join(["'"+item+"'" for item in POC_List])})'''
-    print("Querying...")
-    results = conn.execute(sa.text(sql_query)).fetchall()
-    print("Finished Query")
+        # Get the column names
+        inspector = sa.inspect(engine)
+        columns = inspector.get_columns('FinalEnergyPrices')
+        column_names = [column['name'] for column in columns]
 
-# Get the column names
-inspector = sa.inspect(engine)
-columns = inspector.get_columns('FinalEnergyPrices')
-column_names = [column['name'] for column in columns]
+        # filepath
+        output_file = 'C:/Users/bryce/OneDrive - The University of Auckland/PersonalProjects/Query outputs/OTABEN_PRICES.csv'
 
-# filepath
-output_file = 'C:/Users/bryce/OneDrive - The University of Auckland/PersonalProjects/Query outputs/OTABEN_PRICES.csv'
+        # Write the results to CSV
+        with open(output_file, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            # Write the column headers using inspector
+            csvwriter.writerow(column_names)
+            # Write the data rows
+            csvwriter.writerows(results)
 
-# Write the results to CSV
-with open(output_file, 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    # Write the column headers using inspector
-    csvwriter.writerow(column_names)
-    # Write the data rows
-    csvwriter.writerows(results)
+    if ALLNODES:
+        # Set start End Dates
+        Start_Date = '2022-01-01'
+        End_Date = '2024-01-01'
+
+        # Query
+        with engine.begin() as conn:
+            # For now let us only take Otahuhu and Benmore
+            sql_query = f'''
+            SELECT *
+            FROM FinalEnergyPrices
+            WHERE TradingDate BETWEEN '{Start_Date}' AND '{End_Date}'
+            '''
+            print("Querying...")
+            results = conn.execute(sa.text(sql_query)).fetchall()
+            print("Finished Query")
+
+        # Get the column names
+        inspector = sa.inspect(engine)
+        columns = inspector.get_columns('FinalEnergyPrices')
+        column_names = [column['name'] for column in columns]
+
+        # filepath
+        output_file = 'C:/Users/bryce/OneDrive - The University of Auckland/PersonalProjects/Query outputs/2022-2023AllNodes.csv'
+
+        # Write the results to CSV
+        with open(output_file, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            # Write the column headers using inspector
+            csvwriter.writerow(column_names)
+            # Write the data rows
+            csvwriter.writerows(results)
